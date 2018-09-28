d67c4e7844ac217dead715d4a84bb7e069911546
Thomas Buß <tbuss93@gmail.com>
2018-09-28 12:19:27 +0200

    Renamed Controller

--- src/main/kotlin/de/adesso/samplesck/EnvironmentVariableController.kt
+++ src/main/kotlin/de/adesso/samplesck/EnvironmentVariableController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class TestController {
+class EnvironmentVariableController {
 
     @Value("\${SOME_ENV_VAR}")
     var envVar: String = ""
